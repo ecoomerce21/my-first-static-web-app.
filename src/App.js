5df2eef8dc051d4b3e1cccd5f8e761949c68cfb7
ecoomerce21 <139957881+ecoomerce21@users.noreply.github.com>
2025-04-21 22:15:52 +0500

    App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Creator from "./pages/Creator";
+import Consumer from "./pages/Consumer";
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/creator" element={<Creator />} />
+        <Route path="/consumer" element={<Consumer />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
