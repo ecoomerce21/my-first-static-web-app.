8c0deca132e8f921d33f7b55619b18423e1f3358
ecoomerce21 <139957881+ecoomerce21@users.noreply.github.com>
2025-04-21 22:17:33 +0500

    Create Creator.js

---
+++ src/pages/Creator.js
@@ -0,0 +1,11 @@
+import React from "react";
+import CreatorUpload from "../components/CreatorUpload";
+
+export default function Creator() {
+  return (
+    <div>
+      <h1>Creator Dashboard</h1>
+      <CreatorUpload />
+    </div>
+  );
+}
