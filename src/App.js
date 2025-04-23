69d015fbffb6767a117d7de3ff6ed70155ca4d2b
ecoomerce21 <139957881+ecoomerce21@users.noreply.github.com>
2025-04-23 18:22:04 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Creator from "./pages/Creator";
-import Consumer from "./pages/Consumer";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Creator from './pages/Creator';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/creator" element={<Creator />} />
-        <Route path="/consumer" element={<Consumer />} />
       </Routes>
     </Router>
   );
