d7b804933465d22b6fe74379ad5c670bf0525c80
ecoomerce21 <139957881+ecoomerce21@users.noreply.github.com>
2025-04-27 11:34:19 +0500

    Delete src/pages directory

--- src/pages/Creator.js
+++
@@ -1,3 +0,0 @@
-export default function Creator() {
-  return <h2>Upload your photo – Creator View</h2>;
-}
--- src/pages/Home.js
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-  return <h2>Welcome to CloudGram – Consumer View</h2>;
-}
